567b841d263b07aa174654697f250b2ec8ddce26
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2023-10-28 17:10:48 +0600

    loading state

---
+++ src/components/Loading/Loading.jsx
@@ -0,0 +1,15 @@
+import LoadingImg from "../../assets/loading.gif";
+const Loading = () => {
+  const style = {
+    background: `radial-gradient(circle, rgba(255,255,255,0) 0%, rgba(255,255,255,1) 30%)`,
+  };
+
+  return (
+    <div className="relative flex justify-center items-center h-screen w-full">
+      <div style={style} className=" h-full w-full absolute top-0"></div>
+      <img src={LoadingImg} alt="" />
+    </div>
+  );
+};
+
+export default Loading;
--- src/pages/Dashboard/Seller/MyProducts/MyProducts.jsx
+++ src/pages/Dashboard/Seller/MyProducts/MyProducts.jsx
@@ -1,10 +1,11 @@
+import Loading from "../../../../components/Loading/Loading";
 import getProductsByEmail from "../../../../hooks/getProductsByEmail";
 
 const MyProducts = () => {
     const [products, isLoading] = getProductsByEmail();
     console.log(products);
     if(isLoading){
-        return <h2>Loading...</h2>
+        return <Loading></Loading>
     }
     return (
         <div>
