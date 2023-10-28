c2a624c1eb3ecd4ab0c59b6830241adccf54fda5
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2023-10-28 10:16:55 +0600

    search and filter option design in products page

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -18,9 +18,52 @@ const Products = () => {
           <span className="font-semibold">/</span> Products
         </p>
       </div>
-      
+
       {/* Page Content Start */}
-      Products
+      <div className="my-24">
+        {/* search and filter option */}
+        <div className="md:flex text-center md:px-24">
+          <div className="input-group pl-14 md:pl-0 pb-4 md:pb-0">
+            <input
+              type="text"
+              placeholder="Search…"
+              // ref={searchRef}
+              className="input input-bordered rounded-md bg-black"
+            />
+            <button className="btn btn-square border-none bg-black">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6 "
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                />
+              </svg>
+            </button>
+          </div>
+          <div className="">
+            <label className="block mb-2 text-lg">
+              <span className="">Category</span>
+            </label>
+            <select className=" px-3 py-3 bg-[#000000a4] rounded-sm focus:outline-none">
+              <option disabled selected>
+                default
+              </option>
+              <option>Apple</option>
+              <option>Samsung</option>
+              <option>Oppo</option>
+            </select>
+          </div>
+        </div>
+
+        {/* all products list */}
+      </div>
     </div>
   );
 };
