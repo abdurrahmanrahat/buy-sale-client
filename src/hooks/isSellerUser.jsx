36fe97b16f9061d52a1078e9f1c36ebf1437423b
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2023-10-31 16:08:03 +0600

    seller user fixed

--- src/hooks/isSellerUser.jsx
+++ src/hooks/isSellerUser.jsx
@@ -2,25 +2,33 @@ import { useContext } from "react";
 import { AuthContext } from "../provider/AuthProvider";
 import axios from "axios";
 import { useQuery } from "react-query";
+import useUsers from "./useUsers";
 
 const isSellerUser = () => {
   const { user } = useContext(AuthContext);
+  const [users] = useUsers();
 
-  const {
-    data: isSeller,
-    refetch,
-    isLoading,
-  } = useQuery({
-    queryKey: ["seller"],
-    queryFn: async () => {
-      const res = await axios(
-        `http://localhost:5000/seller?email=${user?.email}`
-      );
-      return res.data;
-    },
-  });
+  const findUser = users?.find((u) => u.email === user?.email);
 
-  return [isSeller, isLoading];
+  console.log(findUser);
+  const isSeller = findUser?.role === "seller";
+
+  // const {
+  //   data: isSeller,
+  //   refetch,
+  //   isLoading,
+  // } = useQuery({
+  //   queryKey: ["seller"],
+  //   queryFn: async () => {
+  //     const res = await axios(
+  //       `http://localhost:5000/seller?email=${user?.email}`
+  //     );
+  //     return res.data;
+  //   },
+  // });
+  // console.log(isSeller);
+
+  return [isSeller];
 };
 
 export default isSellerUser;
--- src/layout/Dashboard.jsx
+++ src/layout/Dashboard.jsx
@@ -12,6 +12,8 @@ const Dashboard = () => {
   const [isBuyer] = isBuyerUser();
   const [isAdmin] = isAdminUser();
 
+  console.log(isSeller);
+
   return (
     <div className="drawer lg:drawer-open">
       <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
