5f887c996fad45b62aa79b8f165adb2eb3cdfe20
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2023-11-22 18:48:34 +0600

    gallery section updated

--- src/pages/Home/GallerySection/GallerySection.jsx
+++ src/pages/Home/GallerySection/GallerySection.jsx
@@ -1,6 +1,3 @@
-import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
-import "react-tabs/style/react-tabs.css";
-
 import Iphone1 from "../../../assets/gallery/iphone1.png";
 import Iphone2 from "../../../assets/gallery/iphone2.png";
 import Iphone3 from "../../../assets/gallery/iphone3.png";
@@ -11,12 +8,14 @@ import Oppo2 from "../../../assets/gallery/oppo2.png";
 import Oppo3 from "../../../assets/gallery/oppo3.png";
 import Oppo4 from "../../../assets/gallery/oppo4.png";
 
+import { useState } from "react";
 import Samsung1 from "../../../assets/gallery/samsung1.png";
 import Samsung2 from "../../../assets/gallery/samsung2.png";
 import Samsung3 from "../../../assets/gallery/samsung3.png";
 import Samsung4 from "../../../assets/gallery/samsung4.png";
 
 const GallerySection = () => {
+  const [activeBtn, setActiveBtn] = useState("apple");
   return (
     <div className="mt-20 mb-12">
       <div className="text-center md:px-28 mb-10">
@@ -31,109 +30,146 @@ const GallerySection = () => {
         </p>
       </div>
 
-      {/* react tab */}
+      {/* button here */}
+      <div className="flex justify-center items-center gap-1 md:gap-4">
+        <button
+          className={`text-[12px] md:text-[18px] font-semibold bg-[#EE9322] w-[80px] md:w-[100px] hover:bg-black hover:text-[#ddd] duration-700 py-[6px] rounded border border-transparent hover:border-white ${
+            activeBtn === "all" && "bg-white text-[#000]"
+          }`}
+          onClick={() => setActiveBtn("all")}
+        >
+          All
+        </button>
+
+        <button
+          className={`text-[12px] md:text-[18px] font-semibold bg-[#EE9322] w-[80px] md:w-[100px] hover:bg-black hover:text-[#ddd] duration-700 py-[6px] rounded border border-transparent hover:border-white ${
+            activeBtn === "apple" && "bg-white text-[#000]"
+          }`}
+          onClick={() => setActiveBtn("apple")}
+        >
+          Apple
+        </button>
+
+        <button
+          className={`text-[12px] md:text-[18px] font-semibold bg-[#EE9322] w-[80px] md:w-[100px] hover:bg-black hover:text-[#ddd] duration-700 py-[6px] rounded border border-transparent hover:border-white ${
+            activeBtn === "oppo" && "bg-white text-[#000]"
+          }`}
+          onClick={() => setActiveBtn("oppo")}
+        >
+          Oppo
+        </button>
+
+        <button
+          className={`text-[12px] md:text-[18px] font-semibold bg-[#EE9322] w-[80px] md:w-[100px] hover:bg-black hover:text-[#ddd] duration-700 py-[6px] rounded border border-transparent hover:border-white ${
+            activeBtn === "samsung" && "bg-white text-[#000]"
+          }`}
+          onClick={() => setActiveBtn("samsung")}
+        >
+          Samsung
+        </button>
+      </div>
+
       <div className="px-4 md:px-0">
-        <Tabs>
-          <TabList>
-            <Tab>
-              <h4 className="font-semibold">All</h4>
-            </Tab>
-            <Tab>
-              <h4 className="font-semibold">Apple</h4>
-            </Tab>
-            <Tab>
-              <h4 className="font-semibold">Oppo</h4>
-            </Tab>
-            <Tab>
-              <h4 className="font-semibold">Samsung</h4>
-            </Tab>
-          </TabList>
+        {/* all photo */}
+        <div>
+          {activeBtn === "all" && (
+            <>
+              <div className="grid grid-cols-2 md:grid-cols-4 gap-6 py-10">
+                <div>
+                  <img src={Iphone1} className="rounded-md" alt="" />
+                </div>
+                <div>
+                  <img src={Iphone2} className="rounded-md" alt="" />
+                </div>
 
-          {/* All List */}
-          <TabPanel>
-            <div className="grid grid-cols-2 md:grid-cols-4 gap-6 py-10">
-              <div>
-                <img src={Iphone1} className="rounded-md" alt="" />
+                <div>
+                  <img src={Iphone4} className="rounded-md" alt="" />
+                </div>
+                <div>
+                  <img src={Oppo2} className="rounded-md" alt="" />
+                </div>
+                <div>
+                  <img src={Oppo3} className="rounded-md" alt="" />
+                </div>
+                <div>
+                  <img src={Oppo1} className="rounded-md" alt="" />
+                </div>
+                <div>
+                  <img src={Oppo2} className="rounded-md" alt="" />
+                </div>
+                <div>
+                  <img src={Oppo3} className="rounded-md" alt="" />
+                </div>
               </div>
-              <div>
-                <img src={Iphone2} className="rounded-md" alt="" />
-              </div>
+            </>
+          )}
+        </div>
 
-              <div>
-                <img src={Iphone4} className="rounded-md" alt="" />
+        {/* apple photo */}
+        <div>
+          {activeBtn === "apple" && (
+            <>
+              <div className="grid grid-cols-2 md:grid-cols-4 gap-6 py-10">
+                <div>
+                  <img src={Iphone1} className="rounded-md" alt="" />
+                </div>
+                <div>
+                  <img src={Iphone2} className="rounded-md" alt="" />
+                </div>
+                <div>
+                  <img src={Iphone3} className="rounded-md" alt="" />
+                </div>
+                <div>
+                  <img src={Iphone4} className="rounded-md" alt="" />
+                </div>
               </div>
-              <div>
-                <img src={Oppo2} className="rounded-md" alt="" />
-              </div>
-              <div>
-                <img src={Oppo3} className="rounded-md" alt="" />
-              </div>
-              <div>
-                <img src={Oppo1} className="rounded-md" alt="" />
-              </div>
-              <div>
-                <img src={Oppo2} className="rounded-md" alt="" />
-              </div>
-              <div>
-                <img src={Oppo3} className="rounded-md" alt="" />
-              </div>
-            </div>
-          </TabPanel>
+            </>
+          )}
+        </div>
 
-          {/* Apple */}
-          <TabPanel>
-            <div className="grid grid-cols-2 md:grid-cols-4 gap-6 py-10">
-              <div>
-                <img src={Iphone1} className="rounded-md" alt="" />
+        {/* oppo photo */}
+        <div>
+          {activeBtn === "oppo" && (
+            <>
+              <div className="grid grid-cols-2 md:grid-cols-4 gap-6 py-10">
+                <div>
+                  <img src={Oppo1} className="rounded-md" alt="" />
+                </div>
+                <div>
+                  <img src={Oppo2} className="rounded-md" alt="" />
+                </div>
+                <div>
+                  <img src={Oppo3} className="rounded-md" alt="" />
+                </div>
+                <div>
+                  <img src={Oppo4} className="rounded-md" alt="" />
+                </div>
               </div>
-              <div>
-                <img src={Iphone2} className="rounded-md" alt="" />
-              </div>
-              <div>
-                <img src={Iphone3} className="rounded-md" alt="" />
-              </div>
-              <div>
-                <img src={Iphone4} className="rounded-md" alt="" />
-              </div>
-            </div>
-          </TabPanel>
+            </>
+          )}
+        </div>
 
-          {/* Oppo */}
-          <TabPanel>
-            <div className="grid grid-cols-2 md:grid-cols-4 gap-6 py-10">
-              <div>
-                <img src={Oppo1} className="rounded-md" alt="" />
+        {/* samsung photo */}
+        <div>
+          {activeBtn === "samsung" && (
+            <>
+              <div className="grid grid-cols-2 md:grid-cols-4 gap-6 py-10">
+                <div>
+                  <img src={Samsung1} className="rounded-md" alt="" />
+                </div>
+                <div>
+                  <img src={Samsung2} className="rounded-md" alt="" />
+                </div>
+                <div>
+                  <img src={Samsung3} className="rounded-md" alt="" />
+                </div>
+                <div>
+                  <img src={Samsung4} className="rounded-md" alt="" />
+                </div>
               </div>
-              <div>
-                <img src={Oppo2} className="rounded-md" alt="" />
-              </div>
-              <div>
-                <img src={Oppo3} className="rounded-md" alt="" />
-              </div>
-              <div>
-                <img src={Oppo4} className="rounded-md" alt="" />
-              </div>
-            </div>
-          </TabPanel>
-
-          {/* Samsung */}
-          <TabPanel>
-            <div className="grid grid-cols-2 md:grid-cols-4 gap-6 py-10">
-              <div>
-                <img src={Samsung1} className="rounded-md" alt="" />
-              </div>
-              <div>
-                <img src={Samsung2} className="rounded-md" alt="" />
-              </div>
-              <div>
-                <img src={Samsung3} className="rounded-md" alt="" />
-              </div>
-              <div>
-                <img src={Samsung4} className="rounded-md" alt="" />
-              </div>
-            </div>
-          </TabPanel>
-        </Tabs>
+            </>
+          )}
+        </div>
       </div>
     </div>
   );
